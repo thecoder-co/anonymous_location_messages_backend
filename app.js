c574af9b2db1218dc368ae8b0519aaecf7960854
thecoder-co <thecoder.co@gmail.com>
2023-08-28 10:43:16 +0100

    cors import

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp'); // Prevent parameter pollution
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
