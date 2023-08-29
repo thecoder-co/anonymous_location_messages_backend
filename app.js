499b7e9a6db60361d65ee183d9a77bb6c4319e34
ABIMBOLA, Idunnuoluwa <thecoder.co@gmail.com>
2023-08-29 15:10:10 +0100

    removed rate limit

--- app.js
+++ app.js
@@ -26,13 +26,13 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Limit requests from same API
-const limiter = rateLimit({
-  max: 1000,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour',
-});
+// const limiter = rateLimit({
+//   max: 1000,
+//   windowMs: 60 * 60 * 1000,
+//   message: 'Too many requests from this IP, please try again in an hour',
+// });
 
-app.use('/api', limiter);
+// app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
